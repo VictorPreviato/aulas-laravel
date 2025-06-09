f3d228470db66fd3cd5c696d524c8e415da2e94c
Victor Previato <victor.gpreviato@senacsp.edu.br>
2025-06-09 11:43:23 -0300

    primeiro commit

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,5 @@ Route::get('/', function () {
 });
 
 Route::get('/sobrenos', function(){
-    return 'Essa é a página sobre nós da Dotme';
-});
-Route::get('/contato', function(){
-    return 'Essa é a página de contatos da Dotme';
-});
-Route::get('/apoie', function(){
-    return 'Essa é a página apoie o projeto da Dotme';
-});
-Route::get('/faq', function(){
-    return 'Essa é a página de questões frequentemente feitas para a Dotme';
-});
-Route::get('/redessocias', function(){
-    return 'Essa é a página de redes sociais da Dotme';
+    return 'Essa é a página sobre nós';
 });
